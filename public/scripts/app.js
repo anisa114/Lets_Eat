cc14a98dc1c3fdd081bd9c55091b26617bb1d136
tkorignal <tauqueer.khan@gmail.com>
2019-01-31 23:18:44 +0000

    Working on menu styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,21 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+
+const addToCart = (element) => {
+  let btnEl = $(element)
+  let quantity = btnEl.siblings('.quantity').val();
+  let name = btnEl.siblings('.menu__item-name').text();
+  let description = btnEl.siblings('.menu__item-description').text();
+  let price = btnEl.siblings('.menu__item-price').text();
+  console.log(quantity, name, description, price)
+}
+
