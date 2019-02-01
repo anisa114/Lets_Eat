bb52818f45f5cd106a1eff04b175397cc6f82e30
tkorignal <tauqueer.khan@gmail.com>
2019-02-01 20:45:52 +0000

    Completed functionality of adding to local storage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,38 +1,56 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
 //Creating a temp array that stores cart items
 var cart = [];
 const addToCart = (element) => {
-  let btnEl = $(element)
-  let quantity = btnEl.siblings('.quantity').val();
-  let name = btnEl.siblings('.menu__item-name').text();
-  let description = btnEl.siblings('.menu__item-description').text();
-  let price = btnEl.siblings('.menu__item-price').text();
-  console.log(quantity, name, description, price)
+  const btnEl = $(element)
+  const id = btnEl.siblings('.menu__item-id').text();
+  const quantity = parseInt(btnEl.siblings('.quantity').val());
+  const name = btnEl.siblings('.menu__item-name').text();
+  const description = btnEl.siblings('.menu__item-description').text();
+  const price = btnEl.siblings('.menu__item-price').text();
+  let item = { id, name, description, quantity, price };
 
-  let item = {
-    name: name,
-    description: description,
-    price:price,
-    quantity: quantity
+  if (!JSON.parse(localStorage.getItem('items'))) {
+    cart.push(item);
+    localStorage.setItem('items', JSON.stringify(cart));
+    return;
+  }
+  const ls = JSON.parse(localStorage.getItem('items'));
+  if (isPresent (ls, name)) {
+    cart = ls.map(item => {
+      if (item.name === name) {
+        item.quantity += quantity
+        return item
+      }
+      return item;
+    })
+    localStorage.setItem('items', JSON.stringify(cart))
+    return 
   }
   cart.push(item);
-  console.log(cart);
+  localStorage.setItem('items', JSON.stringify(cart))
 }
 
+function isPresent (data, itemName) {
+  return data.filter(item => item.name === itemName).length > 0 ? true : false
+}
+function subTotal () {
+  const orders = JSON.parse(localStorage.getItem('items'));
+  return orders.reduce((acc, cur) => {
+    return acc + (cur.price * cur.quantity)
+  }, 0)
+}
+
+function salesTax () {
+  return subTotal() * .13;
+}
+
+function totalPrice () {
+  return subTotal() + salesTax();
+}
 //Adding click event listener to cart button
 $( "#cart" ).click(function() {
   //Persist the cart array to to Local storage
   localStorage.setItem('items', JSON.stringify(cart));
-
 });
 //Remove item from cart 
 function removeItem(name){
@@ -43,7 +61,7 @@ function removeItem(name){
     }
   });
   return items;
-}
+
 //Update quantity of item in cart
 function updateItem(name, newQuantity){
   let items = JSON.parse(localStorage.getItem('items'));
@@ -78,4 +96,4 @@ function handleClick(submit){
     url: "/restaurants/cart",
   
   });
-}
+};
